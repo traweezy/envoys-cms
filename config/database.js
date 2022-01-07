e1f5f1c57aa21ac573661db32b26d27a93dd6b86
traweezy <tyler.schumacher@protonmail.com>
2022-01-07 13:54:11 -0500

    update db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const parse = require("pg-connection-string").parse;
-const config =
-  "postgres://lavgfahbdrhavg:927323450df10aab0072f5a738f4fb3e4871e3b88ffcf3f1a67dd1fbc245ac58@ec2-52-73-8-82.compute-1.amazonaws.com:5432/de65jolt2qqsd7";
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
