e5e622c867cff36634dcbba7d0e0379b19f50c4e
traweezy <tyler.schumacher@protonmail.com>
2022-01-08 05:57:50 -0500

    add aws config

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'yourBucketName.s3.yourRegion.amazonaws.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'yourBucketName.s3.yourRegion.amazonaws.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'envoys.s3.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'envoys.s3.amazonaws.com'],
           upgradeInsecureRequests: null,
         },
       },
--- config/plugins.js
+++ config/plugins.js
@@ -1,30 +1,16 @@
-module.exports = ({ env }) => {
-    console.log( {
-   provider: 'aws-s3',
-   providerOptions: {
-     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-     secretAccessKey: env('AWS_ACCESS_SECRET'),
-     region: env('AWS_REGION'),
-     params: {
-       Bucket: env('AWS_BUCKET_NAME'),
-        StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
-     },
-     logger: console // Only if you want to `stdout` logs
-   }
- })
-   return  ({
+module.exports = ({ env }) =>  ({
  upload: {
-   provider: 'aws-s3',
-   providerOptions: {
-     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-     secretAccessKey: env('AWS_ACCESS_SECRET'),
-     region: env('AWS_REGION'),
-     params: {
-       Bucket: env('AWS_BUCKET_NAME'),
-        StorageClass: env('AWS_S3_STORAGE_CLASSES') // Configure your Amazon S3 Storage Classes (including this to environment variable is not a must)
-     },
-     logger: console // Only if you want to `stdout` logs
-   }
- }
+     config: {
+        provider: 'aws-s3',
+        providerOptions: {
+            accessKeyId: 'AKIAZ2M7JULJHLPY3VXX',
+            secretAccessKey: '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',
+            region: 'us-east-1',
+            params: {
+            Bucket: 'envoys',
+            },
+            logger: console // Only if you want to `stdout` logs
+        }
+     }
+    }
 })
-};
