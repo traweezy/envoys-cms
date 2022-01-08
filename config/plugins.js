2108c8f20232a5fef5b09944c7167691f23cdff9
traweezy <tyler.schumacher@protonmail.com>
2022-01-08 08:15:10 -0500

    add aws config

--- config/plugins.js
+++ config/plugins.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) =>  ({
             secretAccessKey: env('AWS_ACCESS_SECRET'),
             region: env('AWS_REGION'),
             params: {
-                Bucket: env('AWS_BUCKET'),
+                Bucket: env('AWS_BUCKET_NAME'),
             },
             logger: console // Only if you want to `stdout` logs
         }
